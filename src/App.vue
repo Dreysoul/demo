120ec099bc656181f75184808cb87d15b20df782
Dreysoul <183114052@qq.com>
2022-10-10 23:50:10 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<mySchool></mySchool>
+</template>
+
+<script>
+import mySchool from './components/mySchool.vue';
+
+export default {
+  name: 'App',
+  components:{
+    mySchool,
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/mySchool.vue
@@ -0,0 +1,17 @@
+<template>
+  <button>{{ msg }}</button>
+</template>
+
+<script>
+export default {
+  name:'mySchool',
+  data() {
+    return {
+      msg:'hello'
+    };
+  },
+};
+</script>
+
+<style>
+</style>
